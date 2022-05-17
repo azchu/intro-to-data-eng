db5b059c33c7430e4e1e314b2d971867b191fbe8
azchu <79515543+azchu@users.noreply.github.com>
2022-05-18 00:06:31 +1000

    Added correlation matrixes and simple data visualisation

--- ardd_fatalities_2011_2021.m
+++ ardd_fatalities_2011_2021.m
@@ -1,42 +1,60 @@
-% data = readcell("ardd_fatalities_month.xlsx", "UseExcel", true, "Sheet", "ARDD");
-% [NUMERIC, TXT, RAW] = xlsread("ardd_fatalities_mar2022.xlsx", 2);
-%% Declaring each variable
-% state = data(:, 2);
-% month = data(:, 3);
-% year = data(:, 4);
-% dayweek = data(:, 5);
-% time = data(:, 6);
-% crashtype = data(:, 7);
-% bus = data(:, 8);
-% heavyTruck = data(:, 9);
-% artTruck = data(:, 10);
-% speed = data(:, 11);
-% user = data(:, 12);
-% gender = data(:, 13);
-% age = data(:, 14);
-% christmas = data(:, 19);
-% easter = data(:, 20);
-% timeOfDay = data(:, 23);
+%% Exporting the dataset
 [NUMERIC, TXT, RAW] = xlsread("ardd_fatalities_2011_2021.xlsx", 1);
-% RAW([1, 2, 3, 4],:) = [];
-% NUMERIC([1, 2, 3, 4],:) = [];
 data = cell2table(RAW);
-% dataNumeric = cell2table(NUMERIC);
+
+%% Setting variable names
 data.Properties.VariableNames = ["State", "Month", "Year", "Dayweek", "Time", "Crash Type", "Bus Involvement", "Heavy Rigid Truck Involvement", "Articulated Truck Involvement", "Speed Limit", "Road User", "Gender", "Age", "National Remoteness Areas", "SA4 Name 2016", "National LGA Name 2017", "National Road Type", "Christmas Period", "Easter Period", "Age Group", "Day of week", "Time of day"];
-% NUMERIC = NUMERIC(["State", "Month", "Year", "Dayweek", "Time", "Crash Type", "Bus Involvement", "Heavy Rigid Truck Involvement", "Articulated Truck Involvement", "Speed Limit", "Road User", "Gender", "Age", "National Remoteness Areas", "SA4 Name 2016", "National LGA Name 2017", "National Road Type", "Christmas Period", "Easter Period", "Age Group", "Day of week", "Time of day"]);
+
 %% Data Pre-Processing
 % Missing values:
 % Interval/Numeric - replaced by median value
-% data.("Speed Limit")(data.("Speed Limit") == -9) = nan;
-data{:, "Speed Limit"}(data{:, "Speed Limit"} == -9)= nan;
-% data.("Speed Limit")(isEqual(data.("Speed Limit"), -9)) = nan;
 medianSpeedLimit = median(data.("Speed Limit"), "omitnan");
 data.("Speed Limit")(isnan(data.("Speed Limit"))) = medianSpeedLimit;
+medianAge = median(data.Age, "omitnan");
+data.Age(isnan(data.Age)) = medianAge;
 
-%  function to convert to a numerical array
-% filteredData = data(:, {'Age', 'Gender', 'Time of day','Dayweek', 'Month', 'Year', 'State', 'Speed Limit', 'Christmas Period', 'Easter Period', 'Road User'});
- filteredData = data(:, {'Age', 'Month', 'Year', 'Speed Limit'});
-%% Correlation Matrix
-cm = corr(filteredData{:, :});
+%% Correlation Matrix for Numeric attributes
+% Getting numeric attributes
+numericData = data(:, {'Age', 'Month', 'Year', 'Speed Limit'});
 
+% Creating correlation matrix
+corrNumeric = corr(numericData{:, :});
 
+%% Correlation Matrix for Suitable Categorical attributes
+% gender, day week, easter, christmas
+
+% Convert attributes to unique numbers (based on value)
+catGender = double(categorical(data.Gender));
+catDayWeek = double(categorical(data.Dayweek));
+catChristmas = double(categorical(data.("Christmas Period")));
+catEaster = double(categorical(data.("Easter Period")));
+
+% Add attributes to the same matrix
+categoricalData = [catGender, catDayWeek, catChristmas, catEaster];
+% Remove rows where gender is equal to "Unknown" (3)
+categoricalData(categoricalData(catGender) == 3, :) = [];
+
+% Creating correlation matrix
+corrCategorical = corr(categoricalData);
+
+%% Death vs state barplot
+[SC,SR] = groupcounts(data.State); 
+bar(groupcounts(data.State));
+title("Deaths per state");
+ylabel("Death Count");
+xlabel("State");
+set(gca, 'XTickLabel',{"ACT",'NSW','NT','Qld','SA','TAS','VIC','WA'})
+
+%% Deaths vs year barplot
+[YC,YR] = groupcounts(data.Year);
+c  = polyfit(YR, YC, 1);
+xfit = polyval(c, YR);
+plot(YR,YC,'r--o', YR, xfit,'b','MarkerFaceColor', 'r');
+title("Deaths per year");
+ylabel("Death Count (per person)");
+xlabel("Year");
+
+%% Pie chart for male vs female deaths
+pieGender = groupcounts(data.Gender);
+pieGender(3) = [];
+pie(pieGender, {'Female','Male'});
